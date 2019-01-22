3962fdb66c6ee4ebb2dd0eaa76f43711a3767bb5
Ram9199 <sriramscience7@gmail.com>
2019-01-23 01:56:53 +0530

    Chef : Add missing props for Chef
    * An attempt to fix Volte
    
    Signed-off-by: Ram9199 <sriramscience7@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -98,6 +98,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.radio.force_on_dc=true \
+    persist.radio.data_con_rprt=1 \
+    persist.vendor.radio.data_ltd_sys_ind=1 \
+    persist.radio.data_ltd_sys_ind=1 \
+    persist.vendor.radio.ignore_dom_time=10 \
+    persist.radio.ignore_dom_time=10 \
+    persist.radio.is_wps_enabled=true \
+    persist.radio.videopause.mode=1 \
+    persist.radio.sap_silent_pin=1 \
+    persist.radio.always_send_plmn=true \
+    persist.rcs.supported=1 \
+    persist.dbg.ims_volte_enable=1 \
     persist.vendor.radio.no_wait_for_card=1 \
     persist.vendor.radio.dfr_mode_set=1 \
     persist.vendor.radio.relay_oprt_change=1 \
